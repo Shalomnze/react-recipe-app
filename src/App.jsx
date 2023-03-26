60905831f5b6da8468ff5758c7d2061198905145
Shalomnze <nzeugochukwu4@gmail.com>
2023-03-26 19:32:28 +0100

    React recipe app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Pages from "./pages/Pages"
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+      
+      <Pages />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Vegetable from '../components/Vegetable'
+import Popular from '../components/Popular'
+
+const Home = () => {
+  return (
+    <div>
+        <Vegetable />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
